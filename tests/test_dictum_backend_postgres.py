d9328b575d69935ffe50db08e4df891e534a74a1
Mikhail Akimov <rovinj.akimov@gmail.com>
2022-08-02 19:46:55 +0300

    add plugin entrypoint, test that plugin works

--- tests/test_dictum_backend_postgres.py
+++ tests/test_dictum_backend_postgres.py
@@ -1,5 +1,11 @@
+from dictum_core.backends.base import Backend
+
 from dictum_backend_postgres import __version__
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_plugin():
+    assert "postgres" in Backend.registry
