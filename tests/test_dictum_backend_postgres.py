7fa1ee2da648e85e7885629a2a0a572f5f0e3291
Mikhail Akimov <roveo@users.noreply.github.com>
2022-08-02 22:52:14 +0300

    add plugin entrypoint, test (#3)
    Add plugin entrypoint and tests for it

--- tests/test_dictum_backend_postgres.py
+++ tests/test_dictum_backend_postgres.py
@@ -1,5 +1,17 @@
+import shlex
+import subprocess
+
 from dictum_backend_postgres import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
+
+
+def test_entry_point():
+    subprocess.check_call(
+        shlex.split(
+            "python -c 'from dictum_core.backends.base import Backend; "
+            'assert "postgres" in Backend.registry\''
+        )
+    )
