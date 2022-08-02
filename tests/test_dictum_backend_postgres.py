6b502f29a0db7c70593f5099c78704aff8bc3f6b
Mikhail Akimov <rovinj.akimov@gmail.com>
2022-08-02 22:15:22 +0300

    add plugin def, bump version [skip ci]

--- tests/test_dictum_backend_postgres.py
+++ tests/test_dictum_backend_postgres.py
@@ -1,11 +1,17 @@
-from dictum_core.backends.base import Backend
+import shlex
+import subprocess
 
 from dictum_backend_postgres import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
 
 
-def test_plugin():
-    assert "postgres" in Backend.registry
+def test_entry_point():
+    subprocess.check_call(
+        shlex.split(
+            "python -c 'from dictum_core.backends.base import Backend; "
+            'assert "postgres" in Backend.registry\''
+        )
+    )
